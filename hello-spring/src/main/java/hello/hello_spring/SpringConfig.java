d112b7ebfb90d1069c2ae3d973f892983040c1b6
munncoding <cjfgus983@gmail.com>
2025-03-17 20:06:52 +0900

    6. 오리지널 고대 JDBC API

--- hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
@@ -1,29 +1,43 @@
-//package hello.hello_spring;
-///*
-//*
-//*  자바 코드로 직접 스프링 빈에 컴포넌트들 등록하기
-//*
-//* */
-//
-//import hello.hello_spring.repository.MemberRepository;
-//import hello.hello_spring.repository.MemoryMemberRepository;
-//import hello.hello_spring.service.MemberService;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-///*
-//*
-//* 멤버 리포지토리를 생성해서 멤버 서비스에 넣어줌
-//*
-//* */
-//@Configuration
-//public class SpringConfig {
-//    @Bean
-//    public MemberService memberService(){
-//        return new MemberService(memberRepository());
-//    }
-//
-//    @Bean
-//    public MemberRepository memberRepository(){
-//        return new MemoryMemberRepository();
-//    }
-//}
+package hello.hello_spring;
+/*
+*
+*  자바 코드로 직접 스프링 빈에 컴포넌트들 등록하기
+*
+* */
+
+import hello.hello_spring.repository.JdbcMemberRepository;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+import hello.hello_spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/*
+*
+* 멤버 리포지토리를 생성해서 멤버 서비스에 넣어줌
+*
+* */
+@Configuration
+public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository(){
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
+    }
+}
